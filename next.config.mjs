e1b67405cd38808474a086c62c6559797120286a
seming2 <popqr3@naver.com>
2024-11-29 23:42:48 +0900

    도메인 주소 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ['media.rawg.io'], // 허용할 도메인 추가
     remotePatterns: [
       {
         protocol: 'https',
